680a92889331169d1692c192f6d340b4d4f1c4e0
Maria Jenkins <macjenkin@gmail.com>
2023-10-04 10:05:49 -0500

    edit insert.sql

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 27, 'M', 'brown', 'stately', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'brown', 'rowdy', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'brown', 'annoying', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'brown', 'prim', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'gray', 'feisty', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'black', 'grumpy', TRUE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'F', 'gray', 'nervous', FALSE);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'F', 'white', 'temperamental', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 27, 'M', 'brown', 'stately', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'brown', 'rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'brown', 'annoying', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'brown', 'prim', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'gray', 'feisty', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'black', 'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'F', 'gray', 'nervous', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'F', 'white', 'temperamental', 1);
